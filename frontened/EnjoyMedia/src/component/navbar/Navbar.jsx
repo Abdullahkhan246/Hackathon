77de26375803d8a955b6d365877422e8ca663d92
Abdullahkhan246 <anittawilson50@gmail.com>
2025-09-29 12:17:04 +0500

    project Responsive

--- frontened/EnjoyMedia/src/component/navbar/Navbar.jsx
+++ frontened/EnjoyMedia/src/component/navbar/Navbar.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.css'
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div>
        <nav className="navbar">
         <Link to={"/home"} className='logo'>Enjoy Media</Link>
-        <div className="nav-links">
-          <Link to={"/home"} className='nav-links-item'>Home</Link>
-          <Link to={"/leaderboard"} className='nav-links-item'>Leaderboard</Link>
-          <Link to={"/createpost"} className='nav-links-item'>Create Post</Link>
-          <Link to={"/postlist"} className='nav-links-item'>PostList</Link>
+        
+        {/* Hamburger menu button */}
+        <div className="hamburger" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        
+        {/* Navigation links with responsive class */}
+        <div className={`nav-links ${menuOpen ? 'active' : ''}`}>
+          <Link to={"/home"} className='nav-links-item' onClick={toggleMenu}>Home</Link>
+          <Link to={"/leaderboard"} className='nav-links-item' onClick={toggleMenu}>Leaderboard</Link>
+          <Link to={"/createpost"} className='nav-links-item' onClick={toggleMenu}>Create Post</Link>
+          <Link to={"/postlist"} className='nav-links-item' onClick={toggleMenu}>PostList</Link>
         </div>
       </nav>
     </div>
